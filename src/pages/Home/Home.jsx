a31f4030b28519cac0cede05c0a4c5217dc71a5a
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-22 19:08:50 +0530

    feat: add SpecialOffer section to Home page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 import Banner from '../../components/Banner/Banner.jsx';
+import SpecialOffer from '../../components/SpecialOffer/SpecialOffer.jsx';
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Banner />
+      <SpecialOffer />
     </>
   );
 };
