e29796615ef4574ada8a1091dd0200a3fa70e390
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-23 15:26:15 +0530

    add AboutHome rounting

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Navbar from '../../components/Navbar/Navbar.jsx';
 import Banner from '../../components/Banner/Banner.jsx';
 import SpecialOffer from '../../components/SpecialOffer/SpecialOffer.jsx';
+import AboutHome from '../../components/AboutHome/AboutHome.jsx'
 
 const Home = () => {
   return (
@@ -9,6 +10,8 @@ const Home = () => {
       <Navbar />
       <Banner />
       <SpecialOffer />
+      <AboutHome />
+      
     </>
   );
 };
