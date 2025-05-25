60718c60cb3f63e2df545cc377aa40d13fe5c21c
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-25 20:54:32 +0530

    add footer ,ourhomemenu and files routing

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,6 +3,8 @@ import Navbar from '../../components/Navbar/Navbar.jsx';
 import Banner from '../../components/Banner/Banner.jsx';
 import SpecialOffer from '../../components/SpecialOffer/SpecialOffer.jsx';
 import AboutHome from '../../components/AboutHome/AboutHome.jsx'
+import OurHomeMenu from '../../components/OurHomeMenu/OurHomeMenu.jsx';
+import Footer from '../../components/Footer/Footer.jsx';
 
 const Home = () => {
   return (
@@ -11,6 +13,8 @@ const Home = () => {
       <Banner />
       <SpecialOffer />
       <AboutHome />
+      <OurHomeMenu />
+      <Footer />
       
     </>
   );
