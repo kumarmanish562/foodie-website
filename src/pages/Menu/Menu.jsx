27700ed54e2bdd7b27b330076ea005197b93d797
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-25 20:57:43 +0530

    add footer and ouemenu files routing

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar.jsx'
+import Footer from '../../components/Footer/Footer.jsx'
+import OurMenu from '../../components/OurMenu/OurMenu.jsx'
 const Menu = () => {
   return (
     <>
-      <Navbar />
+      <Navbar />  
+      <OurMenu />
+      <Footer />
     </>
   )
 }
