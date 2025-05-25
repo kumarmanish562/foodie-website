def20f666f2aa42b4cdc144fe55ecf862cf72ae6
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-25 20:46:12 +0530

    feat: Configure routes with PrivateRoute protection for secure pages
    - Added route definitions for Home, Contact, About, Menu, Cart, Login, and Signup
    - Secured Contact, About, Menu, and Cart routes using PrivateRoute
    - Ensured only authenticated users can access protected pages

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,37 @@ import AboutPage from './pages/AboutPage/AboutPage'
 import Menu from './pages/Menu/Menu'
 import Cart from './pages/Cart/Cart'
 import SignUp from './components/SignUp/SignUp'
+import PrivateRoute from './components/PrivateRoute/PrivateRoute'
 
 
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/contact' element={< ContactPage />} />
-      <Route path='/about' element={ <AboutPage />} />
-      <Route path='/menu' element={ <Menu />} />
-      <Route path='/cart' element={ < Cart />} />
+      <Route path='/contact' element={
+        <PrivateRoute>
+            < ContactPage />
+        </PrivateRoute>
+        } />
+      <Route path='/about' element={ 
+        <PrivateRoute>
+             <AboutPage />
+        </PrivateRoute>
+       } />
+      <Route path='/menu' element={ 
+        <PrivateRoute>
+            <Menu />
+        </PrivateRoute>
+        } />
+      
       <Route path='/login' element={ < Home />} />
       <Route path='/signup'element={ <SignUp />} />
+
+      <Route path='/cart' element={ 
+        <PrivateRoute>
+            < Cart />
+        </PrivateRoute>
+        } />
     </Routes>
   )
 }
