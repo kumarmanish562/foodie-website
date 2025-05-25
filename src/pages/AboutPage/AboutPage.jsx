403769edaff3bf6964858ca2bf7671a497a09e66
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-25 20:48:46 +0530

    add about and footer route

--- src/pages/AboutPage/AboutPage.jsx
+++ src/pages/AboutPage/AboutPage.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar'
+import Footer from '../../components/Footer/Footer'
+import About from '../../components/About/About'
 
 const AboutPage = () => {
   return (
     <>
       <Navbar />
+      <About />
+      < Footer />
     </>
   )
 }
