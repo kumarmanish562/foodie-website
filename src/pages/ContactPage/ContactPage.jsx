700b17234e3e654893e0b76b18700ef9b8e49947
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-25 20:51:42 +0530

    add about and footer route

--- src/pages/ContactPage/ContactPage.jsx
+++ src/pages/ContactPage/ContactPage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import Navbar from '../../components/Navbar/Navbar.jsx'
+import Footer from '../../components/Footer/Footer.jsx'
+import Contact from '../../components/Contact/Contact.jsx'
 const ContactPages = () => {
   return (
     <>
       <Navbar />
+      <Contact />
+      < Footer />
     </>
   )
 }
