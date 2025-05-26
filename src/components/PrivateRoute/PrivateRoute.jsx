1704359fda72816a868c8a6dd17c148068019535
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-26 22:35:01 +0530

    nothing

--- src/components/PrivateRoute/PrivateRoute.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-
-const PrivateRoute = ({ children }) => {
-  const isAuthenticated = Boolean(localStorage.getItem('loginData'));
-  return isAuthenticated ? children : <Navigate to="/login" replace />;
-};
-
-export default PrivateRoute;
