0988b73441ae8e90e3165c8d89dfb27aa9a7f3e2
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-27 10:16:13 +0530

    removed all files

--- backend/config/db.js
+++
@@ -1,6 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async() => {
-  await mongoose.connect('mongodb+srv://ailogintools:m&mmeals562@cluster0.rtsur9f.mongodb.net/m&mmeals')
-  .then(() => console.log('DB CONNECTED'))
-}
