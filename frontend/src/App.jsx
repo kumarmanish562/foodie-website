8fe07dab4d763f781499d382973bdcc83e34e0c9
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-28 20:23:57 +0530

    Adds checkout and order management routes
    Introduces new routes for checkout and order management, including `CheckoutPage` and `MyOrderPage`, while ensuring the existing routes are properly encapsulated within `PrivateRoute` for secure access.
    
    Refines the structure by removing redundant lines and maintaining clarity in the routing setup.
    
    Improves overall code organization and enhances user navigation within the application.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,32 +7,23 @@ import Menu from './pages/Menu/Menu'
 import Cart from './pages/Cart/Cart'
 import SignUp from './components/SignUp/SignUp'
 import PrivateRoute from './components/PrivateRoute/PrivateRoute'
+import VerifyPaymentPage from './pages/VerifyPaymentPage/VerifyPaymentPage'
+import CheckoutPage from './pages/CheckoutPage/CheckoutPage'
+import MyOrderPage from './pages/MyOrderPage/MyOrderPage'
 const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
       <Route path='/contact' element={< ContactPage />} />
-      <Route path='/about' element={ <AboutPage />} />
-      <Route path='/menu' element={ 
-        <PrivateRoute>
-            <Menu />
-        </PrivateRoute>
-        } />
-      <Route path='/login' element={ < Home />} />
-      <Route path='/signup'element={ <SignUp />} />
-
-
-      {/* pAYMENT VERFICATION PAGE */}
-
-      <Route path='/myorder/verify' element={<VerifyPaymentPage />} />
-
-
-
-      <Route path='/cart' element={ 
-        <PrivateRoute>
-            < Cart />
-        </PrivateRoute>
-        } />
+      <Route path='/about' element={<AboutPage />} />
+      <Route path='/login' element={< Home />} />
+      <Route path='/signup' element={<SignUp />} />
+      {/* PAYMENT VERFICATION PAGE */}
+      <Route path='/myorder/verify' element={ <VerifyPaymentPage />} />
+      <Route path='/menu' element={ <PrivateRoute> <Menu /> </PrivateRoute>} />
+      <Route path='/cart' element={ <PrivateRoute> < Cart /> </PrivateRoute>} />
+      <Route path='/checkout' element={ <PrivateRoute> <CheckoutPage /> </PrivateRoute>} />
+      <Route path='/myorder' element={ <PrivateRoute> <MyOrderPage /> </PrivateRoute>} />
     </Routes>
   )
 }
