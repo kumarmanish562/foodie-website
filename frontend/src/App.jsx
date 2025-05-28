06fd4a3c76d405d2a9e84d3f6a091cdc9721233d
kumarmanish562 <qmanishkumar562@gmail.com>
2025-05-28 20:17:08 +0530

    feat: add VerifyPaymentPage component and clean up route definitions
    Adds VerifyPaymentPage and cleans up routes
    
    Introduces the VerifyPaymentPage component to enhance payment verification functionality.
    
    Also refines route definitions for better readability by consolidating whitespace.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -11,12 +11,8 @@ const App = () => {
   return (
     <Routes>
       <Route path='/' element={<Home />} />
-      <Route path='/contact' element={
-            < ContactPage />
-        } />
-      <Route path='/about' element={ 
-             <AboutPage />
-       } />
+      <Route path='/contact' element={< ContactPage />} />
+      <Route path='/about' element={ <AboutPage />} />
       <Route path='/menu' element={ 
         <PrivateRoute>
             <Menu />
@@ -24,6 +20,14 @@ const App = () => {
         } />
       <Route path='/login' element={ < Home />} />
       <Route path='/signup'element={ <SignUp />} />
+
+
+      {/* pAYMENT VERFICATION PAGE */}
+
+      <Route path='/myorder/verify' element={<VerifyPaymentPage />} />
+
+
+
       <Route path='/cart' element={ 
         <PrivateRoute>
             < Cart />
